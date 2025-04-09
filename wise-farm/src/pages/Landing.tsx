2489c72a5d81034ddfec0068f6e635e6c4798812
ruxlsr <russelsonwa1@gmail.com>
2025-04-09 19:27:05 +0100

    base page

---
+++ wise-farm/src/pages/Landing.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export const Landing = (props?: Props) => {
+  return <div>Landing</div>;
+};
---
+++ wise-farm/src/pages/Meteo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export const Meteo = (props?: Props) => {
+  return <div>Meteo</div>;
+};
